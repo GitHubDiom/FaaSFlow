f4b7f957fc98146fc6218023ecc7e06ad3baef3e
vagrant <vagrant@Dataflower-dev-01>
2023-10-12 13:43:38 +0000

    sudo run scripts

--- scripts/db_setup.bash
+++ scripts/db_setup.bash
@@ -1,6 +1,6 @@
 # install docker
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -9,9 +9,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose python3-pip
-apt-get install wondershaper
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose python3-pip
+sudo apt-get install wondershaper
 pip3 install -r requirements.txt
 # install and initialize couchdb
 docker pull couchdb
--- scripts/worker_setup.bash
+++ scripts/worker_setup.bash
@@ -1,6 +1,6 @@
 # install docker
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -9,8 +9,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io python3-pip
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io python3-pip
 # install python packages
 pip3 install -r requirements.txt
 # install redis
